711ebc4e008ac87754e49ceb08de12a44fd8803d
Taylor Swift <vl.priyavishnu15@gmail.com>
2024-07-04 16:59:52 +0000

    Looping through slidebar and shoppigcart icon

--- FirstPr/Productspage.spec.ts
+++ FirstPr/Productspage.spec.ts
@@ -50,8 +50,90 @@ test('To loop through slide bars', async({page}) =>{
   await cross.click();
   await expect(hamburger).toBeVisible();
 
+
+  
+
+});
+
+test('checking the About of the slidebars', async({page}) =>{
+
+  const hamburger = page.locator('.bm-burger-button');
+    await hamburger.click();
+    
+
+  const about = page.locator('//*[@id="about_sidebar_link"]');
+
+  await about.click();
+  await page.isVisible("search");
+
+
+
+});
+
+
+test('checking Logout in the slidebars', async({page}) =>{
+
+  const hamburger = page.locator('.bm-burger-button');
+    await hamburger.click();
+    
+
+  const logout = page.locator('//*[@id="logout_sidebar_link"]');
+
+  await logout.click();
+  await page.isVisible("login_logo");
+
+
+
+});
+
+
+test('checking resetappstate in the slidebars', async({page}) =>{
+
+  const hamburger = page.locator('.bm-burger-button');
+    await hamburger.click();
+    
+
+  const resetappstate = page.locator('//*[@id="reset_sidebar_link"]');
+
+  await resetappstate.click();
+  await page.isVisible("login_logo");
+
+
+
 });
 
 
 
 
+
+test('checking shoppingcart in the slidebars', async({page}) =>{
+
+  const shoppingcart = page.locator('.shopping_cart_link');
+    await shoppingcart.click();
+    
+
+    const checkout = page.locator("//button[@id='checkout']");
+
+
+
+  await checkout.click();
+  await page.isVisible("continue");
+
+  //const cbutton = page.locator("//button[@id='continue']");
+
+  const cbutton = page.locator("#continue");
+
+  // Wait for the button to be visible and enabled before clicking
+  await cbutton.waitFor({ state: 'visible', timeout: 30000 });
+  //await cbutton.waitFor({ state: 'enabled', timeout: 30000 });
+
+
+  await cbutton.click();
+  
+
+  await page.waitForSelector("//div[@id='checkout_info_container']//form//h3", { timeout: 10000 });
+
+  // Assert the visibility of the element
+  await expect(page.locator("//div[@id='checkout_info_container']//form//h3")).toBeVisible();
+
+});
